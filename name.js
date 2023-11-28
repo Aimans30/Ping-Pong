80d583c00b14916df6e76dceacaa2cc880ab137f
Aimans30 <145761115+Aimans30@users.noreply.github.com>
2023-11-28 14:32:49 +0530

    Add files via upload

---
+++ name.js
@@ -0,0 +1,14 @@
+
+
+function letsGo(){
+    let name=document.getElementById("name").value;
+    let nickName=document.getElementById("nickname").value;
+    
+    let obj={
+        name,
+        nickName
+    }
+    console.log(obj)
+    localStorage.setItem("userDetails",JSON.stringify(obj));
+    location.href="./game.html"
+}
